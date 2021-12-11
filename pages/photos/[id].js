1f337ad780a85eb15bff281aaebc70d6cf51215f
Md Sakin Hossain <info.sakinh@gmail.com>
2021-12-12 00:46:12 +0600

    tried but pagination doesn't work

--- pages/photos/[id].js
+++ pages/photos/[id].js
@@ -4,7 +4,6 @@ import React from 'react';
 import style from "/styles/Home.module.css";
 
 const Photo = ({ photo }) => {
-    console.log(photo);
     return (
         <div className={style.container}>
             <h1>User</h1>
@@ -29,22 +28,17 @@ export async function getStaticPaths() {
     const res = await fetch(`https://api.unsplash.com/photos/random/?client_id=GFbWxYGsOUeW6YtBrIhPzR4NFIhxckRADLFhyk0ZCug&count=10`)
     const photos = await res.json();
   
-    // Get the paths we want to pre-render based on posts
     const paths = photos.map((photo) => ({
       params: { id: photo.id.toString() },
     }))
-  
-    // We'll pre-render only these paths at build time.
-    // { fallback: false } means other routes should 404.
+
     return { paths, fallback: true }
 }
 
   export async function getStaticProps({ params }) {
-    // params contains the post `id`.
-    // If the route is like /posts/1, then params.id is 1
+
     const res = await fetch(`https://api.unsplash.com/photos/${params.id}/?client_id=GFbWxYGsOUeW6YtBrIhPzR4NFIhxckRADLFhyk0ZCug`)
     const photo = await res.json()
   
-    // Pass post data to the page via props
     return { props: { photo } }
 }
--- pages/photos/index.js
+++ pages/photos/index.js
@@ -1,9 +1,12 @@
 import Image from 'next/image';
 import Link from 'next/link';
+import Router from 'next/router';
 import React from 'react';
 import style from "/styles/Home.module.css";
 
-const index = ({photos}) => {
+const index = ({photos, page}) => {
+    console.log(photos);
+    console.log(page);
     return (
         <div className={style.container}>
             <h1>Click on Image to get details</h1>
@@ -19,21 +22,27 @@ const index = ({photos}) => {
                     </Link>
                 </div>
             )
-        }
+        } 
+
+        <button onClick={()=> Router.push(`?page=${page+1}&per_page=${3}&client_id=GFbWxYGsOUeW6YtBrIhPzR4NFIhxckRADLFhyk0ZCug`)}>Next Page</button>
         </div>
     );
 };
 
 export default index;
 
+
+
 export async function getStaticProps(){
-    const count = 10;
+    let page;
+    { params:{id: page=1} }
     const apiKey = "GFbWxYGsOUeW6YtBrIhPzR4NFIhxckRADLFhyk0ZCug"
-    const res = await fetch(`https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`)
+    const res = await fetch(`https://api.unsplash.com/photos?page=${page}&per_page=${3}&client_id=${apiKey}`);
     const photos = await res.json();
     return{
       props: {
-        photos
+        photos,
+        page
       }
     }
   }
